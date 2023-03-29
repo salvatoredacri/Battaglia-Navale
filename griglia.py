692fb73b46bc6b454b7f876500dbbb8e3b39717a
Frise99 <93764048+Frise99@users.noreply.github.com>
2023-03-29 17:22:29 +0200

    Update griglia.py

--- griglia.py
+++ griglia.py
@@ -8,16 +8,32 @@ def crea_griglia():
 
     #creazione della griglia
     griglia = [[0] * n_colonne for _ in range(n_righe)]
-#richiesta all'utente di posizionare le navi
+   
+    
+    nome_nave = input("Inserisci il nome della nave: ")
+    lunghezza_nave = None
     for ship in ship_list:
-     print("Inserisci le navi di nome {ship.nome}(lunghezza {lunghezza.lunghezza})")
-    for i in range(numero_navi[ship]):
-     while True:
-         orientamento = input("Inserisci l'orientamento della nave (orizzontale o verticale): ")
-         riga = int(input("Inserisci la riga in cui inserire la nave: "))
-         colonna = int(input("Inserisci la colonna in cui inserire la nave: "))
-         if puo_inserire_nave(ship.lunghezza, orientamento, riga, colonna):
-             break
-         else:
-             print("Non puoi inserire la nave qui. Riprova.")
+          if ship.nome == nome_nave:
+           lunghezza_nave = ship.lunghezza
+           print(f"La nave {nome_nave} ha lunghezza {lunghezza_nave}.")
+           break
+    if lunghezza_nave is None:
+            print("Nome della nave non valido.")   
+#richiesta all'utente di posizionare le navi
+   for ship in ship_list:
+        while True:
+         stampa_griglia(griglia, n_righe, n_colonne)
+         try:
+          orientamento = str(input("Inserisci l'orientamento della nave (orizzontale o verticale): "))
+          riga = int(input("Inserisci la riga in cui inserire la nave: "))
+          colonna = int(input("Inserisci la colonna in cui inserire la nave: ")) 
+          if Navi.puo_inserire_nave(ship.lunghezza, orientamento, riga, colonna, griglia): 
+            Navi.inserisci_nave(ship.lunghezza, orientamento, riga, colonna, griglia)
+            break
+         except IndexError:
+          print("Inserisci una coordinata valida. Riprova.")
+    else:
+         print("Non puoi inserire la nave qui. Riprova.")
+             
+
     return griglia
