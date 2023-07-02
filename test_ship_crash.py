a4140ebe1d203f3b83445549d1de9c2296a9c5bb
Marcello Esposito <esposito.marce@gmail.com>
2023-07-02 11:57:53 +0200

    More tests are available

--- test_ship_crash.py
+++ test_ship_crash.py
@@ -172,3 +172,88 @@ class TestShipCrash(unittest.TestCase):
         first = Navi("first", 5, 'v', 0, 'A', None)
         second = Navi("second", 5, 'v', 0, 'C', None)
         self.assertFalse(first.crashes_against(second))
+
+    def test_completely_overlapped_horizontal_5len_ships_crash(self):
+        """
+            A B C D E F G H I
+          1 0 0 0 0 0 0 0 0 0
+          2 0 0 0 0 0 0 0 0 0
+          3 0 0 x x x x x 0 0
+          4 0 0 0 0 0 0 0 0 0
+          5 0 0 0 0 0 0 0 0 0
+          6 0 0 0 0 0 0 0 0 0
+          7 0 0 0 0 0 0 0 0 0
+          8 0 0 0 0 0 0 0 0 0
+          9 0 0 0 0 0 0 0 0 0
+        """
+        first_ship = Navi('first', 5, 'o', 2, 'C', None)
+        second_ship = Navi('second', 5, 'o', 2, 'C', None)
+        self.assertTrue(first_ship.crashes_against(second_ship))        
+
+    def test_partially_overlapped_5len_ships_crash(self):
+        """
+            A B C D E F G H I
+          1 0 0 0 0 0 0 0 0 0
+          2 0 0 0 0 0 0 0 0 0
+          3 0 > > > x x < < <
+          4 0 0 0 0 0 0 0 0 0
+          5 0 0 0 0 0 0 0 0 0
+          6 0 0 0 0 0 0 0 0 0
+          7 0 0 0 0 0 0 0 0 0
+          8 0 0 0 0 0 0 0 0 0
+          9 0 0 0 0 0 0 0 0 0
+        """
+        first_ship = Navi('first', 5, 'o', 2, 'B', None)
+        second_ship = Navi('second', 5, 'o', 2, 'E', None)
+        self.assertTrue(first_ship.crashes_against(second_ship))   
+
+    def test_L_shaped_ships_crash(self):
+        """
+            A B C D E F G H I
+          1 0 0 0 0 > 0 0 0 0
+          2 0 0 0 0 > 0 0 0 0
+          3 0 0 0 0 > 0 0 0 0
+          4 0 0 0 0 > 0 0 0 0
+          5 0 0 0 0 > < < < <
+          6 0 0 0 0 0 0 0 0 0
+          7 0 0 0 0 0 0 0 0 0
+          8 0 0 0 0 0 0 0 0 0
+          9 0 0 0 0 0 0 0 0 0
+        """
+        first_ship = Navi('first', 5, 'v', 0, 'E', None)
+        second_ship = Navi('second', 4, 'o', 4, 'F', None)
+        self.assertTrue(first_ship.crashes_against(second_ship))
+
+    def test_parallel_adjacent_1len_vertical_ships_crash(self):
+        """
+            A B C D E F G H I
+          1 0 0 0 0 > < 0 0 0
+          2 0 0 0 0 0 0 0 0 0
+          3 0 0 0 0 0 0 0 0 0
+          4 0 0 0 0 0 0 0 0 0
+          5 0 0 0 0 0 0 0 0 0
+          6 0 0 0 0 0 0 0 0 0
+          7 0 0 0 0 0 0 0 0 0
+          8 0 0 0 0 0 0 0 0 0
+          9 0 0 0 0 0 0 0 0 0
+        """
+        first_ship = Navi('first', 1, 'v', 0, 'E', None)
+        second_ship = Navi('second', 1, 'v', 0, 'F', None)
+        self.assertTrue(first_ship.crashes_against(second_ship))
+
+    def test_diagonal_1len_non_adjacent_vertical_ships_dont_crash(self):
+        """
+            A B C D E F G H I
+          1 0 0 0 0 > 0 0 0 0
+          2 0 0 0 0 0 0 0 0 0
+          3 0 0 0 0 0 0 < 0 0
+          4 0 0 0 0 0 0 0 0 0
+          5 0 0 0 0 0 0 0 0 0
+          6 0 0 0 0 0 0 0 0 0
+          7 0 0 0 0 0 0 0 0 0
+          8 0 0 0 0 0 0 0 0 0
+          9 0 0 0 0 0 0 0 0 0
+        """
+        first_ship = Navi('first', 1, 'v', 0, 'E', None)
+        second_ship = Navi('second', 1, 'v', 2, 'G', None)
+        self.assertFalse(first_ship.crashes_against(second_ship))
